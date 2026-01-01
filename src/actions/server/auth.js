b0f5a16d89667c458ef62b56e3f9f387edd575ab
Md Robiul Islam <mdrobiulislam1479@gmail.com>
2026-01-01 11:49:56 +0600

    create post user function

---
+++ src/actions/server/auth.js
@@ -0,0 +1,32 @@
+"use server";
+import { dbconnect } from "@/lib/dbConnect";
+import bcrypt from "bcryptjs";
+
+export const postUser = async (payload) => {
+  const { email, password, name } = await payload;
+
+  //check payload
+  if (!email || !password) return null;
+
+  //check user
+  const isExist = await dbconnect("users").findOne({ email });
+  if (isExist) return null;
+
+  //create user
+  const newUser = {
+    provider: "credentials",
+    name,
+    name,
+    password: await bcrypt.hash(password, 14),
+    role: "user",
+  };
+
+  //insert user
+  const result = await dbconnect("users").insertOne(newUser);
+  if (result.acknowledged) {
+    return {
+      ...result,
+      insertedId: result.insertedId.toString(),
+    };
+  }
+};
--- src/lib/dbConnect.js
+++ src/lib/dbConnect.js
@@ -1,9 +1,6 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const uri = process.env.MONGODB_URI;
 const dbname = process.env.DB_NAME;
-const collection = {
-  Products: "products",
-};
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
